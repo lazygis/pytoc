38fbf171f7afaa52b38fea187e5eb94cfa511c18
lazy <zhenliu0711@gmail.com>
2021-08-04 11:00:16 -0400

    TOC painter init

--- pytoc/pytoc.py
+++ pytoc/pytoc.py
@@ -1,5 +1,92 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import transforms
+from matplotlib import ticker as mticker
+from matplotlib import colors as matpltColors
+
+# ---------- This part is for the painting class
+class TOC_paint:
+    def __init__(self, TOC_list, index_names = None, boolUniform = False, boolCorrectCorner = False):
+        if type(TOC_list) != list:
+            self.TOC_list = [TOC_list]
+        else:
+            self.TOC_list = TOC_list
+        if index_names is None:
+            self.index_names = ["TOC{0}".format(i+1) for i in range(len(self.TOC_list))]
+        else:
+            self.index_names = index_names
+        first_TOC = self.TOC_list[0]
+        self.totalNum = first_TOC.TOCX[0, -1]
+        self.presenceInY = first_TOC.TOCY[0, -1]
+        self.TOCNum = len(self.TOC_list)
+        self.boolUniform = boolUniform
+        self.boolCorrectCorner = boolCorrectCorner
+        self.fig = plt.figure()
+
+        cmap = matpltColors.ListedColormap('#e0e0e0')
+        plt.tripcolor([self.totalNum - self.presenceInY, self.totalNum, self.totalNum], [0, 0, self.presenceInY], [0, 1, 2], np.array([1, 1, 1]), edgecolor="k", lw=0,
+                      cmap=cmap)
+        plt.tripcolor([0, 0, self.presenceInY], [0, self.presenceInY, self.presenceInY], [0, 1, 2], np.array([1, 1, 1]), edgecolor="k", lw=0,
+                      cmap=cmap)
+
+        # make the coordinates square
+        plt.axis('square')
+        # plt.axis('auto')
+        plt.axis([0, self.totalNum, 0, self.presenceInY])
+        plt.gca().set_aspect(1 / plt.gca().get_data_ratio())
+    def addAll(self):
+        if (self.boolCorrectCorner):
+            CorrectCornerText1 = plt.gca().text(0, self.presenceInY * 1.01, 'The ', color="black", fontsize=8)
+            CorrectCornerText1.draw(plt.gca().figure.canvas.get_renderer())
+            ex = CorrectCornerText1.get_window_extent()
+            t = transforms.offset_copy(
+                CorrectCornerText1.get_transform(), x=ex.width, units='dots')
+            CorrectCornerText2 = plt.gca().text(0, self.presenceInY * 1.01, '★ ', color="red", fontsize=8, transform=t)
+            CorrectCornerText2.draw(plt.gca().figure.canvas.get_renderer())
+            ex = CorrectCornerText2.get_window_extent()
+            t = transforms.offset_copy(
+                CorrectCornerText2.get_transform(), x=ex.width, units='dots')
+            plt.gca().text(0, self.presenceInY * 1.01, 'marks where False Alarms equals Misses.', color="black", fontsize=8,
+                           transform=t)
+            #     marks where Misses equals False Alarms.
+        if (self.boolUniform):
+            l2 = plt.plot([0, self.totalNum], [0, self.presenceInY], ':', color="violet", label='Uniform')
+        for i in range(self.TOCNum):
+            self.addOne(i, self.index_names[i])
+    def paint(self):
+        plt.show()
+    def addOne(self, index, Name, marker=None):
+        Xlist = self.TOC_list[index].TOCX
+        Ylist = self.TOC_list[index].TOCY
+        if(marker==None):
+            plt.plot(Xlist[0,:], Ylist[0,:], label=Name, picker=2, clip_on=False)
+        else:
+            plt.plot(Xlist[0,:], Ylist[0,:], marker+'-', label=Name, picker=2, clip_on=False)
+        handles, labels = plt.gca().get_legend_handles_labels()
+        numCurve = len(handles)
+        # change the order of maximum and minimum
+        # in handles the order will always uniform, x1, x2
+        if(labels[0]=='Uniform'):
+            order = list(range(1,numCurve))
+            order.extend([0])
+        else:
+            order = list(range(0, numCurve))
+        # put the maximum line first, uniform and minimum at last
+        # order.insert(0,0)
+        # order.extend([1,2])
+        plt.gca().legend([handles[idx] for idx in order], [labels[idx] for idx in order],loc='center left', bbox_to_anchor=(1, 0.5))
+
+
+def drawTOC(TOC_list):
+    ## if the input is only one TOC object, not list.
+    if type(TOC_list)!=list:
+        TOC_list = [TOC_list]
+    first_TOC = TOC_list[0]
+    totalNum = first_TOC.TOCX[0,-1]
+    presenceInY = first_TOC.TOCX[0,-1]
+    TOCNum = len(TOC_list)
+    fig, ax = plt.plot()
+
 
 # ---------- This part is for the use functions for TOC curve
 def AreaUnderCurve(listx, listy):
@@ -11,6 +98,8 @@ def AreaUnderCurve(listx, listy):
     Hlisty = np.delete(listy, 0)
     Areas = np.sum(np.int64([(Hlistx - Llistx) * (Hlisty + Llisty)]), axis=1) / 2.0
     return Areas
+
+
 
 # ---------- This part is the class for TOC curve ----------
 class TOC:
@@ -77,6 +166,14 @@ class TOC:
         self.TOCX = self.TOCX.reshape((1, self.TOCX.shape[0]))
         self.TOCY = np.array(self.TOCY).flatten()
         self.TOCY = self.TOCY.reshape((1, self.TOCY.shape[0]))
+
+    def read_TOC_coor(self, TOCX, TOCY, thresholdLable=np.array([])):
+        self.TOCX = TOCX
+        self.TOCY = TOCY
+        self.thresholdLabel = thresholdLable
+        self.format_coordinates()
+        self.presenceInY = self.TOCY[0, -1]
+        self.totalNum = self.TOCX[0, -1]
 
     def calculate_AUC(self):
         self.format_coordinates()
