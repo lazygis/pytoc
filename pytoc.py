8511db8ce95dd65ed265f0875f8ea388674c6908
lazy <zhenliu0711@gmail.com>
2021-11-03 19:07:35 -0400

    TOC finished, TOC_painter init writing

--- pytoc.py
+++ pytoc.py
@@ -1,12 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import transforms
-from matplotlib import ticker as mticker
+# from matplotlib import transforms
+# from matplotlib import ticker as mticker
 from matplotlib import colors as matpltColors
 
+
 # ---------- This part is for the painting class
-class TOC_paint:
-    def __init__(self, TOC_list, index_names = None, boolUniform = False, boolCorrectCorner = False):
+class TOC_painter:
+    def __init__(self, TOC_list=[], color_list=[], marker_list=[], line_list=[], index_names=None, boolUniform=False, boolCorrectCorner=False):
         # To make one TOC read as a list
         if type(TOC_list) != list:
             self.TOC_list = [TOC_list]
@@ -14,21 +15,24 @@ class TOC_paint:
             self.TOC_list = TOC_list
         # if there is no name updated
         if index_names is None:
-            self.index_names = ["TOC{0}".format(i+1) for i in range(len(self.TOC_list))]
+            self.index_names = ["TOC{0}".format(i + 1) for i in range(len(self.TOC_list))]
         else:
             self.index_names = index_names
         first_TOC = self.TOC_list[0]
+        self.curve_list = []
         self.totalNum = first_TOC.TOCX[0, -1]
         self.presenceInY = first_TOC.TOCY[0, -1]
         self.TOCNum = len(self.TOC_list)
         self.boolUniform = boolUniform
         self.boolCorrectCorner = boolCorrectCorner
         self.fig = plt.figure()
-
+        ## draw grey areas
         cmap = matpltColors.ListedColormap('#e0e0e0')
-        plt.tripcolor([self.totalNum - self.presenceInY, self.totalNum, self.totalNum], [0, 0, self.presenceInY], [0, 1, 2], np.array([1, 1, 1]), edgecolor="k", lw=0,
+        plt.tripcolor([self.totalNum - self.presenceInY, self.totalNum, self.totalNum], [0, 0, self.presenceInY],
+                      [0, 1, 2], np.array([1, 1, 1]), edgecolor="k", lw=0,
                       cmap=cmap)
-        plt.tripcolor([0, 0, self.presenceInY], [0, self.presenceInY, self.presenceInY], [0, 1, 2], np.array([1, 1, 1]), edgecolor="k", lw=0,
+        plt.tripcolor([0, 0, self.presenceInY], [0, self.presenceInY, self.presenceInY], [0, 1, 2], np.array([1, 1, 1]),
+                      edgecolor="k", lw=0,
                       cmap=cmap)
 
         # make the coordinates square
@@ -36,6 +40,12 @@ class TOC_paint:
         # plt.axis('auto')
         plt.axis([0, self.totalNum, 0, self.presenceInY])
         plt.gca().set_aspect(1 / plt.gca().get_data_ratio())
+    def __update_status(self):
+        self.TOCNum = len(self.curve_list)
+        if(self.TOCNum>0):
+            self.totalNum = self.curve_list[0].TOCX[0,-1]
+            self.presenceInY = self.curve_list[0].TOCY[0,-1]
+
     def paint(self):
         if (self.boolCorrectCorner):
             # CorrectCornerText1 = plt.gca().text(0, self.presenceInY * 1.01, 'The ', color="black", fontsize=8)
@@ -50,95 +60,104 @@ class TOC_paint:
             #     CorrectCornerText2.get_transform(), x=ex.width, units='dots')
             # plt.gca().text(0, self.presenceInY * 1.01, 'marks where False Alarms equals Misses.', color="black", fontsize=8,
             #                transform=t)
-            plt.text(0, self.presenceInY * 1.01, 'The red star marks where False Alarms equals Misses.', color="black", fontsize=8)
+            plt.text(0, self.presenceInY * 1.01, 'The red star marks where False Alarms equals Misses.', color="black",
+                     fontsize=8)
             #     marks where Misses equals False Alarms.
         if (self.boolUniform):
             l2 = plt.plot([0, self.totalNum], [0, self.presenceInY], ':', color="violet", label='Uniform')
         for i in range(self.TOCNum):
             self.__addOne(i, self.index_names[i])
         plt.show()
+
     ## This function is private to add cures to the painter
     def __addOne(self, index, Name, marker=None):
         Xlist = self.TOC_list[index].TOCX
         Ylist = self.TOC_list[index].TOCY
-        if(marker==None):
-            plt.plot(Xlist[0,:], Ylist[0,:], label=Name, picker=2, clip_on=False)
+        if (marker == None):
+            plt.plot(Xlist[0, :], Ylist[0, :], label=Name, picker=2, clip_on=False)
         else:
-            plt.plot(Xlist[0,:], Ylist[0,:], marker+'-', label=Name, picker=2, clip_on=False)
+            plt.plot(Xlist[0, :], Ylist[0, :], marker + '-', label=Name, picker=2, clip_on=False)
         handles, labels = plt.gca().get_legend_handles_labels()
         numCurve = len(handles)
         # change the order of maximum and minimum
         # in handles the order will always uniform, x1, x2
-        if(labels[0]=='Uniform'):
-            order = list(range(1,numCurve))
+        if (labels[0] == 'Uniform'):
+            order = list(range(1, numCurve))
             order.extend([0])
         else:
             order = list(range(0, numCurve))
         # put the maximum line first, uniform and minimum at last
         # order.insert(0,0)
         # order.extend([1,2])
-        plt.gca().legend([handles[idx] for idx in order], [labels[idx] for idx in order],loc='center left', bbox_to_anchor=(1, 0.5))
+        plt.gca().legend([handles[idx] for idx in order], [labels[idx] for idx in order], loc='center left',
+                         bbox_to_anchor=(1, 0.5))
+
+
+
+
+    def read_TOC_coor(self, TOCX, TOCY, thresholdLable=np.array([])):
+        self.TOCX = TOCX
+        self.TOCY = TOCY
+        self.thresholdLabel = thresholdLable
+        self.__format_coordinates()
+        self.presenceInY = self.TOCY[0, -1]
+        self.totalNum = self.TOCX[0, -1]
 
 
 def drawTOC(TOC_list):
     ## if the input is only one TOC object, not list.
-    if type(TOC_list)!=list:
+    if type(TOC_list) != list:
         TOC_list = [TOC_list]
     first_TOC = TOC_list[0]
-    totalNum = first_TOC.TOCX[0,-1]
-    presenceInY = first_TOC.TOCX[0,-1]
+    totalNum = first_TOC.TOCX[0, -1]
+    presenceInY = first_TOC.TOCX[0, -1]
     TOCNum = len(TOC_list)
     fig, ax = plt.plot()
 
 
 # ---------- This part is for the use functions for TOC curve
-def AreaUnderCurve(listx, listy):
-    listx = np.array(listx)
-    listy = np.array(listy)
-    Llistx = np.delete(listx, listx.shape[1] - 1)
-    Hlistx = np.delete(listx, 0)
-    Llisty = np.delete(listy, listy.shape[1] - 1)
-    Hlisty = np.delete(listy, 0)
-    Areas = np.sum(np.int64([(Hlistx - Llistx) * (Hlisty + Llisty)]), axis=1) / 2.0
-    return Areas
 
 
 
 # ---------- This part is the class for TOC curve ----------
 class TOC:
     def __init__(self, booleanArray, indexArray, thresholds, maskArray=None, weightsArray=None):
+        ## define boolean and index variables and thresholds
         self.booleanArray = np.array(booleanArray).flatten()
         self.indexArray = np.array(indexArray).flatten()
         self.thresholds = np.array(thresholds).flatten()
+        ## if Users don't assign any mask, the class will assign it as a matrix with all 1
         if (maskArray is None):
-            self.maskArray = np.ones_like(self.booleanArray)
+            self.maskArray = np.ones_like(self.booleanArray).flatten()
         else:
-            self.maskArray = maskArray
+            self.maskArray = np.array(maskArray).flatten()
+        ## if Users don't assign any mask, the class will assign it as a matrix with all 1
         if (weightsArray is None):
-            self.weightsArray = np.ones_like(self.booleanArray)
+            self.weightsArray = np.ones_like(self.booleanArray).flatten()
         else:
-            self.weightsArray = weightsArray
+            self.weightsArray = np.array(weightsArray).flatten()
+        # initialize TOCX and TOCY and AUC
         # TOCX and TOCY
         self.TOCX = np.array([[]])
         self.TOCY = np.array([[]])
         self.AUC = 0
-        # calculate the correctCornerY and totalNum
-        if(booleanArray is not None):
-            self.presenceInY = np.sum(self.booleanArray * self.weightsArray)
-            self.totalNum = np.sum(self.weightsArray)
-        else:
-            self.presenceInY = 0
-            self.totalNum = 0
+        self.correctCornerY = 0
+        ##init threshold labels
         self.thresholdLabel = np.array([])
+        # calculate the correctCornerY and totalNum
+        self.presenceInY = np.sum(self.booleanArray * self.weightsArray)
+        self.totalNum = np.sum(self.weightsArray)
+        ## calculate TOCX, TOCY, threshold labels, Y value of correctCorner, and AUC
+        self.__calculate_TOC()
 
-    def calculate_TOC(self):
+    def __calculate_TOC(self):
         # There is no origin point when the minimum threshold is the same as index minimum
         self.TOCX = []
         self.TOCY = []
         for threshold in self.thresholds:
             # tell whether the threshold is ascending or descending
             if (self.thresholds[-1] < self.thresholds[0]):
-                BoolIndexArray = (self.indexArray>= threshold).astype(
+                BoolIndexArray = (self.indexArray >= threshold).astype(
                     int)  # set value 1 for pixel which is higher than threshold, other pixels are assigned 0
             else:
                 BoolIndexArray = (self.indexArray <= threshold).astype(
@@ -147,13 +166,12 @@ class TOC:
             self.TOCY.append((BoolIndexArray * self.maskArray * self.booleanArray).sum())  # Hits
 
         # add original zero
-        if(self.TOCX[0]!=0):
-            self.TOCX = [0]+self.TOCX
-            self.TOCY = [0]+self.TOCY
+        if (self.TOCX[0] != 0):
+            self.TOCX = [0] + self.TOCX
+            self.TOCY = [0] + self.TOCY
             self.thresholdLabel = np.append(np.array(['origin']), self.thresholds)
         else:
             self.thresholdLabel = np.array(self.thresholds, dtype=np.str_)
-
 
         # when customize the threshold, the upper bond is not achieved, so we need to add it to the threshold labels
         if (self.TOCX[-1] != self.totalNum):
@@ -162,29 +180,42 @@ class TOC:
             self.thresholdLabel = np.append(self.thresholdLabel, np.array(['end']))
         self.TOCY = np.array([self.TOCY])
         self.TOCX = np.array([self.TOCX])
-        self.calculate_AUC()
+        self.__calculate_AUC()
+        self.__format_coordinates()
+        self.correctCornerY = self.__correctCorner(self.TOCX, self.TOCY, self.presenceInY)
 
-        # self.correctCornerY = correctCorner(self.TOCX, self.TOCY, self.presenceInY)
-    def format_coordinates(self):
+    def __format_coordinates(self):
         self.TOCX = np.array(self.TOCX).flatten()
         self.TOCX = self.TOCX.reshape((1, self.TOCX.shape[0]))
         self.TOCY = np.array(self.TOCY).flatten()
         self.TOCY = self.TOCY.reshape((1, self.TOCY.shape[0]))
 
-    def read_TOC_coor(self, TOCX, TOCY, thresholdLable=np.array([])):
-        self.TOCX = TOCX
-        self.TOCY = TOCY
-        self.thresholdLabel = thresholdLable
-        self.format_coordinates()
-        self.presenceInY = self.TOCY[0, -1]
-        self.totalNum = self.TOCX[0, -1]
+    def __correctCorner(self, TOCX, TOCY, presenceInY):
+        bool_presence = (TOCX<=presenceInY).astype(int)
+        corner_index = bool_presence.sum()-1
+        if(corner_index == TOCY.shape[1]-1):
+            y_res = TOCX[0, -1]
+        else:
+            y_res = (TOCY[0,corner_index+1]-TOCY[0,corner_index])*1.0/(TOCX[0,corner_index+1]-TOCX[0,corner_index])*(presenceInY-TOCX[0,corner_index])+TOCY[0,corner_index]*1.0
+        return y_res
 
-    def calculate_AUC(self):
-        self.format_coordinates()
-        presenceInY = self.TOCY[0,-1]
+
+    def __calculate_AUC(self):
+        self.__format_coordinates()
+        presenceInY = self.TOCY[0, -1]
         totalNum = self.TOCX[0, -1]
-        Area01 = AreaUnderCurve(self.TOCX,self.TOCY)
-        Area02 = AreaUnderCurve(np.array([[0, presenceInY]]),np.array([[0, presenceInY]]))
-        Area03 = AreaUnderCurve(np.array([[0, totalNum]]),np.array([[0, presenceInY]]))*2
-        AUC = (Area01-Area02)/(Area03-2*Area02)
+        Area01 = self.__AreaUnderCurve(self.TOCX, self.TOCY)
+        Area02 = self.__AreaUnderCurve(np.array([[0, presenceInY]]), np.array([[0, presenceInY]]))
+        Area03 = self.__AreaUnderCurve(np.array([[0, totalNum]]), np.array([[0, presenceInY]])) * 2
+        AUC = (Area01 - Area02) / (Area03 - 2 * Area02)
         self.AUC = float(AUC)
+
+    def __AreaUnderCurve(self, listx, listy):
+        listx = np.array(listx)
+        listy = np.array(listy)
+        Llistx = np.delete(listx, listx.shape[1] - 1)
+        Hlistx = np.delete(listx, 0)
+        Llisty = np.delete(listy, listy.shape[1] - 1)
+        Hlisty = np.delete(listy, 0)
+        Areas = np.sum(np.int64([(Hlistx - Llistx) * (Hlisty + Llisty)]), axis=1) / 2.0
+        return Areas
